1b88b66c779f58d091204158a048c8f2ae167c98
artm1904 <artmsavchenko@yandex.ru>
2025-08-27 18:30:46 +0300

    Add interface ICommandExecutor for Dependency Injection  into class ServiceTool

---
+++ src/core/Utils/i_command_executor.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <QString>
+#include <QStringList>
+#include <memory>
+
+/**
+ * @brief Интерфейс (абстрактный класс) для исполнителя команд.
+ * @details Позволяет подменять реального исполнителя на мок в тестах.
+ */
+
+class ICommandExecutor {
+   public:
+    virtual ~ICommandExecutor() = default;
+
+    /**
+     * @brief Выполняет команду с повышенными привилегиями.
+     * @param command Команда для выполнения из-под суперпользователя.
+     * @param args Аргументы для команды.
+     * @return Стандартный вывод (stdout) команды.
+     */
+
+    virtual QString SudoExec(const QString& command, QStringList args = {}) = 0;
+    /**
+     * @brief Выполняет команду.
+     * @param command Команда для выполнения.
+     * @param args Аргументы для команды.
+     * @return Стандартный вывод (stdout) команды.
+     */
+    virtual QString Exec(const QString& command, const QStringList& args = {}) = 0;
+};
+
+using CommandExecutorPtr = std::shared_ptr<ICommandExecutor>;
