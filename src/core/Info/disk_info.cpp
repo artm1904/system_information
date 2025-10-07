af12c7d6f726d3242cad56edd9011065904c19ac
artm1904 <artmsavchenko@yandex.ru>
2025-10-07 10:26:36 +0300

    Fix bug in  UpdateDiskInfo in DiskInfo calss, exec fix

--- src/core/Info/disk_info.cpp
+++ src/core/Info/disk_info.cpp
@@ -9,16 +9,20 @@ QList<Disk> DiskInfo::GetDisks() const { return m_disks; }
 
 void DiskInfo::UpdateDiskInfo() {
     try {
-        QStringList lines = m_commandExecutor->Exec("df -Pl").split("\n");
+        QStringList lines = m_commandExecutor->Exec(QString{"df"}, QStringList{"-Pl"}).split("\n");
         if (lines.isEmpty() == false) {
             for (const QString& line : lines) {
                 QStringList fields = line.split(QRegularExpression("\\s+"), Qt::SkipEmptyParts);
                 Disk disk;
+
+                //qDebug() << "Disk lines found:" << fields;
+
                 if (fields.size() >= 6) {
                     disk.size = fields[1].toULongLong();
                     disk.free = fields[2].toULongLong();
                     disk.used = fields[3].toULongLong();
                 }
+
                 m_disks << disk;
             }
         }
